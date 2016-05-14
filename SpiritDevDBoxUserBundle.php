bd1554f766c655b5c059fbe03d8ef1f7ba1fd1d4
Jean Bordat <bordat.jean@gmail.com>
2016-05-14 16:31:40 +0200

    Updating injection

--- SpiritDevDBoxUserBundle.php
+++ SpiritDevDBoxUserBundle.php
@@ -5,4 +5,7 @@ namespace SpiritDev\Bundle\DBoxUserBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SpiritDevDBoxUserBundle extends Bundle {
+    public function getParent() {
+        return 'FOSUserBundle';
+    }
 }
