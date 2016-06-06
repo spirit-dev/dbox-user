c7e66aec1294b942bd0deecf66af6fb63acaf2d5
Jean Bordat <bordat.jean@gmail.com>
2016-06-06 16:02:22 +0200

    Updating service name call

--- Ldap/LdapDriverCore.php
+++ Ldap/LdapDriverCore.php
@@ -6,17 +6,17 @@
  *   /_`_  ._._/___/ | _
  * . _//_//// /   /_.'/_'|/
  *    /
- *
+ *  
  * Since 2K10 until today
- *
+ *  
  * Hex            53 70 69 72 69 74 2d 44 65 76
- *
+ *  
  * By             Jean Bordat
  * Twitter        @Ji_Bay_
  * Mail           <bordat.jean@gmail.com>
- *
+ *  
  * File           LdapDriverCore.php
- * Updated the    16/05/16 12:28
+ * Updated the    06/06/16 16:00
  */
 
 namespace SpiritDev\Bundle\DBoxUserBundle\Ldap;
@@ -76,13 +76,13 @@ class LdapDriverCore {
         $this->container = $container;
 
         // Setting Ldap Connection info
-        $this->ldapHostname = $container->getParameter('spirit_dev_d_box_portal.ldap_driver.driver.host');
-        $this->ldapPort = $container->getParameter('spirit_dev_d_box_portal.ldap_driver.driver.port');
-        $this->ldapDomLogin = $container->getParameter('spirit_dev_d_box_portal.ldap_driver.driver.username');
-        $this->ldapDomPassw = $container->getParameter('spirit_dev_d_box_portal.ldap_driver.driver.password');
+        $this->ldapHostname = $container->getParameter('spirit_dev_d_box_user.ldap_driver.driver.host');
+        $this->ldapPort = $container->getParameter('spirit_dev_d_box_user.ldap_driver.driver.port');
+        $this->ldapDomLogin = $container->getParameter('spirit_dev_d_box_user.ldap_driver.driver.username');
+        $this->ldapDomPassw = $container->getParameter('spirit_dev_d_box_user.ldap_driver.driver.password');
 
         // Setting Ldap Bind info
-        $this->baseDn = $this->container->getParameter('spirit_dev_d_box_portal.ldap_driver.user.basedn');
+        $this->baseDn = $this->container->getParameter('spirit_dev_d_box_user.ldap_driver.user.basedn');
     }
 
     /**
