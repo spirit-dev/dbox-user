78f995553d82a2f173567fb3e948ad2a1fe072e1
Spirit-Dev <bordat.jean@gmail.com>
2016-08-08 10:42:24 +0200

    User synchronization

--- Security/Handler/InteractiveLoginListener.php
+++ Security/Handler/InteractiveLoginListener.php
@@ -16,7 +16,7 @@
  * Mail           <bordat.jean@gmail.com>
  *
  * File           InteractiveLoginListener.php
- * Updated the    05/08/16 16:34
+ * Updated the    08/08/16 10:41
  */
 
 namespace SpiritDev\Bundle\DBoxUserBundle\Security\Handler;
@@ -61,46 +61,69 @@ class InteractiveLoginListener {
         $username = $user->getUsername();
         if ($username != "admin" && $username != "sys") {
 
+            // Some vars
+            $isModified = false;
             // Get current date
             $now = new \DateTime();
 
+            // Get API's
+            $vcsAPI = $this->container->get('spirit_dev_dbox_portal_bundle.api.gitlab');
+            $pmAPI = $this->container->get('spirit_dev_dbox_portal_bundle.api.redmine');
+            $qaAPI = $this->container->get('spirit_dev_dbox_portal_bundle.api.sonar');
+            // Get Users
+            $vcsUser = $vcsAPI->getUser($user);
+            $pmUser = $pmAPI->getIdByUsername($user);
+
+            // VCS - Create Users if not exists
+            if ($user->getGitLabId() == null && !array_key_exists('id', $vcsUser)) {
+                // Create gitlab user
+                $vcsUser = $vcsAPI->createUser($user);
+                $user->setGitLabId($vcsUser['id']);
+                $isModified = true;
+            }
+
+            // PM - Create Users if not exists
+            if ($user->getRedmineId() == null && !is_int($pmUser)) {
+                // Create redmine user
+                $pmUser = $pmAPI->createUser($user);
+                $user->setRedmineId($pmUser->{'id'});
+                $isModified = true;
+            }
+
+            // QA - Create Users if not exists
+            if ($user->getSonarManaged() == null) {
+                // Create Sonar user here
+                $sonarUser = $qaAPI->createUser($user);
+                $user->setSonarManaged(true);
+                $isModified = true;
+            }
+
+
             // Check schedule sync
-            if ($now > $user->getNextSyncDate()) {
+            if ($now > $user->getNextSyncDate() || $user->getNextSyncDate() == null) {
 
-                // Create gitlab user if not already done
-                if ($user->getGitLabId() == null) {
-                    // Create gitlab user
-                    $gitLabUser = $this->container->get('spirit_dev_dbox_portal_bundle.api.gitlab')->createUser($user);
-                    $user->setGitLabId($gitLabUser['id']);
+                // Create / Synchronize gitlab user if not already done
+                if (array_key_exists('id', $vcsUser)) {
+                    $user->setGitLabId($vcsUser['id']);
+                    $isModified = true;
                 }
 
-                // Create redmine user if not already done
-                if ($user->getRedmineId() == null) {
-                    // Create redmine user
-                    $redmineUser = $this->container->get('spirit_dev_dbox_portal_bundle.api.redmine')->createUser($user);
-                    $user->setRedmineId($redmineUser->{'id'});
-                }
-
-                // Create sonar user if not already done
-                if ($user->getSonarManaged() == null) {
-                    // Create Sonar user here
-                    $sonarUser = $this->container->get('spirit_dev_dbox_portal_bundle.api.sonar')->createUser($user);
-                    $user->setSonarManaged(true);
-                }
-
-                // Update DB user if necessary
-                // TODO Change condition
-                if ($user->getGitLabId() != null || $user->getRedmineId() == null || $user->getSonarManaged() == null) {
-                    // Updating next sync date
-                    $nextSyncDate = $now->add(new \DateInterval('P10D'));
-                    $user->setNextSyncDate($nextSyncDate);
-                    // Updating user
-                    $this->userManager->updateUser($user);
+                // PM - Synchronize
+                if (is_int($pmUser)) {
+                    $user->setRedmineId($pmUser);
+                    $isModified = true;
                 }
 
             }
 
-
+            // Update DB user if necessary
+            if ($isModified) {
+                // Updating next sync date
+                $nextSyncDate = $now->add(new \DateInterval('P10D'));
+                $user->setNextSyncDate($nextSyncDate);
+                // Updating user
+                $this->userManager->updateUser($user);
+            }
         }
     }
 }
